3b3b99927364b5e5f850bc7d002c01610d8225e5
gezu2 <gezugudisa@gmail.com>
2024-02-15 08:47:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-
-const express =require("express");
-const cors= require( "cors");
-const dotenv= require( "dotenv");dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
@@ -15,22 +15,22 @@ app.get("/", (req, res) => {
     massage: "Success!",
   });
 });
-app.post("/payment/create",async(req,res)=>{
-    const total= req.query.total
-    if(total >0){
-        const paymentIntent = await stripe.paymentIntents.create({
-    amount:total,
-    currency:"usd",
-   })
-   console.log(paymentIntent);
-   res.status(201).json({clientSecret:paymentIntent.client_secret});
-    } else{
-        res.status(403).json({message:'The total should be more than zero'})
- }
-})
-app.listen(5000,(err)=>{
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    console.log("Stripe Payment Intent:", paymentIntent);
+    res.status(201).json({ clientSecret: paymentIntent.client_secret })
+  } else {
+    res.status(403).json({ message: "The total should be more than zero" });
+  }
+});
+app.listen(4000,(err)=>{
     if(err) throw err;
-    console.log("Amazon Server is running on port 5000,http://localhost:5000");
+    console.log("Amazon Server is running on port 4000,http://localhost:4000");
 })
 
 
